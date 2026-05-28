bb12eeace34e25306c3e54b4c6351e19b31f705a
teja3101 <tejudhanawade31@gmail.com>
2026-05-28 16:23:55 +0530

    day2 Files

---
+++ day2.js
@@ -0,0 +1,31 @@
+let a = 12
+
+let b = 12.45
+
+let c = 5545454554545
+
+let d = 5465365655.23545454
+
+let e = 'tejaswini'
+
+let f = "Teju"
+
+let g = {
+    'Id': 101,
+    'name': 'ram'
+}
+
+let h = null
+
+let i
+
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+console.log(typeof h)
+console.log(typeof i)
