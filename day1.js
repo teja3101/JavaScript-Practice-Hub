780228e0368ab0e6fee8b8836b8b4aeca5d496e7
teja3101 <tejudhanawade31@gmail.com>
2026-05-28 16:33:00 +0530

    changes

--- day1.js
+++ day1.js
@@ -1,7 +1,23 @@
 // a = "Tejaswini"
 // console.log(a)
 
-var a = "Tejaswini"
-console.log(a)
 
-a = ""
+// var (old way - function scoped)
+var a = 31;
+a = 21;                // reassign allowed
+var a = 30;           // redeclare allowed
+console.log(a);
+
+
+// let (modern - block scoped)
+let b = 10;
+b = 20;              // reassign allowed
+// let b = 30;      // redeclare not allowed
+console.log(b);
+
+
+// const (constant - block scoped)
+const c = 63;
+// c = 55;               -not allowed (cannot reassign)
+// const c = 25;         -not allowed (cannot redeclare)
+console.log(c);
